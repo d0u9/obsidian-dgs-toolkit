c5ddf0fa3ca59a0a5ea84f28eb3bb9b627e89090
Douglas Su <doug.su.au@gmail.com>
2026-08-26 16:39:10 +1000

    Add per-view colon block toggle

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -11,4 +11,10 @@ export function registerCommands(plugin: DgsToolkitPlugin): void {
 			);
 		},
 	});
+
+	plugin.addCommand({
+		id: 'toggle-colon-blocks-current-reading-view',
+		name: 'Toggle colon blocks in current reading view',
+		callback: () => plugin.toggleColonBlocksInActiveReadingView(),
+	});
 }
